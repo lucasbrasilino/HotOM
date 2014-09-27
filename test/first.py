087a6fd0a0a4ae815ede6c39431a8e2b6a718b6b
Lucas Brasilino <lucas.brasilino@gmail.com>
2014-09-27 11:14:27 -0300

    * continuing first test development

--- test/first.py
+++ test/first.py
@@ -1,6 +1,7 @@
 from pox.core import core
 from pox.openflow import *
 import pox.lib.packet as pkt
+from HotOM.lib import *
 
 #class HotOMSwitch(object):
 #    def __init__(self,event):
@@ -28,7 +29,15 @@ class FirstTest(object):
 
     def _handle_PacketIn (self, event):
         packet = event.parsed
-        if packet.type == pkt.ARP_TYPE:
+        pkt_icmp = None
+        if event.dpid == 1:
+            if event.port == 1:
+                pkt_eth = packet.find('ethernet')
+                pkt_ip = packet.find('ipv4')
+                pkt_hotom = hotom(net_id="AA:BB:CC", dst="00:00:02",
+                                  src="00:00:01")
+                pkt_eth.payload = pkt_hotom
+                pkt_hotom.payload = pkt_ip
             
 
 def launch():
