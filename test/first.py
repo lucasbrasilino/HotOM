3e8517517d2f1fd5b1d47bc6c656d255020f2623
Lucas Brasilino <lucas.brasilino@gmail.com>
2014-09-27 22:12:44 -0300

    * better first test implementation

--- test/first.py
+++ test/first.py
@@ -1,45 +1,66 @@
 from pox.core import core
-from pox.openflow import *
+import pox.openflow.libopenflow_01 as of
 import pox.lib.packet as pkt
-from HotOM.lib import *
+from HotOM.lib.header import *
 
-#class HotOMSwitch(object):
-#    def __init__(self,event):
+class HotOMSwitch(object):
+    def __init__(self,event):
+        self.connection = event.connection
+        for p in event.ofp.ports:
+            if p.port_no == 65534:
+                self.name = p.name
+                self.hw_addr = p.hw_addr
         
-
 class FirstTest(object):
 
     def __init__(self):
         self.log = core.getLogger()
         core.openflow.addListeners(self)
         self.log.info("FirstTest initialization")
-        self.sw_ports = dict()
+        self.switches = dict()
         self.connections = set()
 
     def debug (self,event):
-        if isinstance(event, ConnectionUp):
-            self.log.debug("dpid: %s" % event.dpid)
-            #self.log.debug("Switch features: %s" % event.ofp)
+        self.log.debug("dpid: %s" % event.dpid)
+           #self.log.debug("Switch features: %s" % event.ofp)
 
     def _handle_ConnectionUp (self, event):
         self.connections.add(event.connection)
-        self.sw_ports[event.dpid] = list()
-        for p in event.ofp.ports:
-            self.sw_ports[event.dpid].append(p.port_no)
+        self.switches[event.dpid] = HotOMSwitch(event)
 
     def _handle_PacketIn (self, event):
+        self.debug(event)
         packet = event.parsed
-        pkt_icmp = None
-        if event.dpid == 1:
-            if event.port == 1:
-                pkt_eth = packet.find('ethernet')
-                pkt_ip = packet.find('ipv4')
-                pkt_hotom = hotom(net_id="AA:BB:CC", dst="00:00:02",
-                                  src="00:00:01")
-                pkt_eth.payload = pkt_hotom
-                pkt_hotom.payload = pkt_ip
+        if packet.find('arp') is not None:
+            self.log.debug("ARP packet")
+            return
+        if event.port == 1:
+            msg = self.encapsulate(packet,2)
+            event.connection.send(msg)
+        if event.port == 2:
+            msg = self.desencapsulate(packet,1)
+            event.connection.send(msg)
             
-
+        #pkt_ip = packet.find('ipv4')
+        #if pkt_ip is not None:
+        #    return
+        #if event.port == 1:
+        #    msg = self.encapsulate(pa
+        #if event.dpid == 1:
+        #    if event.port == 1:
+        #        pkt_hotom = hotom(net_id="AA:BB:CC", dst="00:00:02",
+        #                          src="00:00:01")
+        #        pkt_vlan = pkt.vlan(id=2,eth_type=0x080A)
+        #        pkt_eth.type = pkt.ethernet.VLAN_TYPE
+        #        pkt_eth.payload = pkt_vlan
+        #        pkt_vlan.payload = pkt_hotom
+        #        pkt_hotom.payload = pkt_ip
+        #        msg = of.ofp_packet_out(data=pkt_eth)
+        #        msg.actions.append(of.ofp_action_output(port = 2))
+        #        event.connection.send(msg)
+        #elif event.dpid == 2:
+        #    print "Pacote: %s" % pkt_ip
+                
 def launch():
     core.registerNew(FirstTest)
     
